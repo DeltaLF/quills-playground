82e2ebefcc23d272d83e93d9ba1f064bfa4b1451
Patrick <patricktsaikl@gmail.com>
2026-01-27 20:44:06 +0800

    feat(Route): setup route and nav page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./home/home.component').then(m => m.HomeComponent)
+  },
+  {
+    path: 'native-quill',
+    loadComponent: () => import('./quills-playground/quills-playground.component').then(m => m.QuillsPlaygroundComponent)
+  },
+  {
+    path: 'ngx-quill',
+    loadComponent: () => import('./ngx-quill-playground/ngx-quill-playground.component').then(m => m.NgxQuillPlaygroundComponent)
+  },
+  {
+    path: 'design-tokens',
+    loadComponent: () => import('./token-playground/token-playground.component').then(m => m.TokenPlaygroundComponent)
+  }
+];
---
+++ src/app/ngx-quill-playground/ngx-quill-playground.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ngx-quill-playground',
+  imports: [],
+  templateUrl: './ngx-quill-playground.component.html',
+  styleUrl: './ngx-quill-playground.component.scss'
+})
+export class NgxQuillPlaygroundComponent {
+
+}
